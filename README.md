d06d9149bc09157a321f1a86f7fee194f12470bb
Ilyas Ronef <ilyas@divandesign.biz>
2014-07-13 18:16:46 +0600

    Version 1.5.4: * The “ddTools::sendMail” method is used for sending e-mails. * Be advised! The snippet is now using the “modx.ddTools” 0.13 library!

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ddSendMail
 
+Snippet for sending e-mails.
+___
 Visit the following [link](http://code.divandesign.biz/modx/ddsendmail) to read the documentation, instructions & changelog.
--- ddSendMail.php
+++ ddSendMail.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * ddSendMail.php
- * @version 1.5.3 (2014-04-04)
+ * @version 1.5.4 (2014-07-13)
  * 
  * @desc Snippet for sending e-mails.
+ * 
+ * @uses The library MODX.ddTools 0.13.
  * 
  * @param $email {comma separated string} - Addresses to mail. @required
  * @param $subject {string} - E-mail subject. Default: 'Mail from '.$modx->config['site_url'].
@@ -11,94 +13,28 @@
  * @param $from {string} - Mailer address. Default: 'info@divandesign.ru'.
  * @param $inputName {comma separated string} - “input” tags names from which accepted files are taken. Default: ''.
  * 
- * @link http://code.divandesign.biz/modx/ddsendmail/1.5.3
+ * @link http://code.divandesign.biz/modx/ddsendmail/1.5.4
  * 
  * @copyright 2014, DivanDesign
  * http://www.DivanDesign.biz
  */
 
-if (!isset($email)){
+//Подключаем modx.ddTools
+require_once $modx->config['base_path'].'assets/snippets/ddTools/modx.ddtools.class.php';
+
+if (empty($email)){
 	$modx->logEvent(1, 2, "Parameter 'email' is required. Called on document id ".$modx->documentIdentifier.'.', 'ddSendMail');
-	
+
 	return false;
 }
 
-//Тема письма
-$subject = isset($subject) ? $subject : 'Mail from '.$modx->config['site_url'];
-//Конвертируем тему в base64
-$subject = "=?UTF-8?B?".base64_encode($subject)."?=";
-//От кого
-$from = isset($from) ? $from : 'info@divandesign.ru';
-//Заголовки сообщения
-$headers = "From: $from\r\nMIME-Version: 1.0\r\n";
+if (!isset($text)){
+	$modx->logEvent(1, 2, "Parameter 'text' is required. Called on document id ".$modx->documentIdentifier.'.', 'ddSendMail');
 
-//Разделитель блоков в сообщении
-$bound = 'bound'.md5(time());
-$multipart = "Content-Type: multipart/mixed; boundary = \"".$bound."\"\r\n\r\n--".$bound;
-
-//Добавлеям текст в сообщения
-if(isset($text)){
-	$multipart .= "\r\nContent-Type: text/html; charset=UTF-8 \r\n\r\n".$text."\r\n\r\n--".$bound;
+	return false;
 }
 
-if(isset($inputName)){
-	$inputName = explode(',', $inputName);
-	
-	//Перебираем имена полей с файлами
-	$attachFiles = array();
-	foreach($inputName as $value){
-		//Проверяем находиться ли в POST массив
-		if(is_array($_FILES[$value]['name'])){
-			//Если массив пустой обрываем итерацию
-			if(!$_FILES[$value]['tmp_name'][0]){break;}
-			
-			//Перебираем пост
-			foreach($_FILES[$value]['name'] as $key => $name){
-				//Если нет ошибок
-				if ($_FILES[$value]['error'][$key] == 0){
-					//Добавляем в массив файлы
-					$attachFiles[$name] = fread(fopen($_FILES[$value]['tmp_name'][$key], 'r'), filesize($_FILES[$value]['tmp_name'][$key]));
-				}
-			}
-		}else{
-			//Если массив пустой обрываем итерацию
-			if(!$_FILES[$value]['tmp_name']){break;}
-			//Если нет ошибок
-			if ($_FILES[$value]['error'] == 0){
-				//Если не массив, то добавляем один файл
-				$attachFiles[$_FILES[$value]['name']] = fread(fopen($_FILES[$value]['tmp_name'], 'r'), filesize($_FILES[$value]['tmp_name']));
-			}
-		}
-	}
-	
-	//Перебираем присоединяемые файлы
-	if(!empty($attachFiles)){
-		foreach($attachFiles as $name=>$value){
-			$multipart .= "\r\n".
-				'Content-Type: application/octet-stream; name = "=?UTF-8?B?'.base64_encode($name)."?=\"\r\n".
-				"Content-Transfer-Encoding: base64\r\n\r\n".
-				base64_encode($value)."\r\n\r\n--".$bound;
-		}
-	}
-}
-
-//Добавляем разделитель окончания сообщения
-$headers .= $multipart."--\r\n";
-
-$result = array();
-
-$email = explode(',', $email);
-foreach ($email as $val){
-	//Если адрес валидный
-	if (filter_var($val, FILTER_VALIDATE_EMAIL)){
-		//Отправляем письмо 
-		if(mail($val, $subject, '', $headers)){
-			$result[] = 1;
-		}else{
-			$result[] = 0;
-		}
-	}
-}
+$result = ddTools::sendMail(explode(',', $email), $text, $from, $subject, explode(',', $inputName));
 
 return count($result) > 1 ? json_encode($result) : $result[0];
 ?>
